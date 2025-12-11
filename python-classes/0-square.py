1e675dc196638fe7e9dcb6cd56409ee7d7e4c5fe
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-11 14:55:15 +0000

    Fixed

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-class Square: # Square class
-    def __init__(self, size): # Init method
+class Square:
+    """This is square class using for calculating size"""
+    def __init__(self, size): 
+        """This is identitation method"""
         self.__size = size
