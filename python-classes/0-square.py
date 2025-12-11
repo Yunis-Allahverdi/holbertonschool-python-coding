127dd902023ab5053f19753008fad40ee9a468cd
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-11 15:07:32 +0000

    First

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 class Square:
-    """This is square class using for calculating size"""
-    def Size(self, size): 
-        """This is identitation method"""
+    """
+    A class made for calculating the size of square
+    """
+    def Size(self, size):
+        """
+        Function to take size and made it private
+        Args:
+            size(int): size must be integer
+        Returns:
+            size in a private attribute
+        """
         self.__size = size
+
+
+sq1 = Square()
+sq1.size = 55
