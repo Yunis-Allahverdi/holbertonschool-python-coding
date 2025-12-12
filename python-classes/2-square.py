7f2f63e03e489862d6c258e6211db2abaae6861f
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-12 10:22:34 +0000

    First

---
+++ python-classes/2-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module created for defining squares
+based on private size attribute
+"""
+
+
+class Square:
+    """
+    This class used for defining size
+    of square
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def sizer(self):
+        return self.__size
+
+    def area(self):
+        area = self.__size ** 2
+        return area
+
+    @sizer.setter
+    def sizer(self, size):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+
+sq1 = Square(50)
